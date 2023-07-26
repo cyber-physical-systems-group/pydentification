6f5697909e21b096a934e139806bcee965d64aaf
Krzysztof Zajac <kzajac1997@gmail.com>
2023-07-26 20:14:17 +0200

    [metrics](feat) Add implementation of metrics package

---
+++ pydentification/metrics/__init__.py
@@ -0,0 +1,16 @@
+from .regression import (
+    normalized_mean_absolute_error,
+    normalized_mean_squared_error,
+    normalized_root_mean_square_error,
+    regression_metrics,
+    regression_report,
+)
+
+# only allow importing metric functions
+__all__ = [
+    "normalized_mean_squared_error",
+    "normalized_root_mean_square_error",
+    "normalized_mean_absolute_error",
+    "regression_metrics",
+    "regression_report",
+]
