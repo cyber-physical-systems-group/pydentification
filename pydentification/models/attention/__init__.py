b524cc5c6e30581ea60715cb1d3d3284564528c6
Krzysztof Zajac <kzajac1997@gmail.com>
2023-08-12 18:59:43 +0200

    [models](feat) Add embeddings and readout modules

--- pydentification/models/attention/__init__.py
+++ pydentification/models/attention/__init__.py
@@ -1,7 +1,12 @@
+from .embeddings import LinearEmbedding
 from .ff import DelayLineFeedforward, TransformerFeedforward
-
+from .readouts import LinearReadout
+from .sa import DynamicalSelfAttention
 
 __all__ = [
     "DelayLineFeedforward",
+    "DynamicalSelfAttention",
+    "LinearEmbedding",
+    "LinearReadout",
     "TransformerFeedforward"
 ]
