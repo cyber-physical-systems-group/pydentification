f37a3781f80d4e807a6e4da7a3bd5f9c02614be5
Krzysztof Zajac <kzajac1997@gmail.com>
2023-08-14 19:34:54 +0200

    [models](feat) Add DynamicalSelf Attention block

--- pydentification/models/attention/__init__.py
+++ pydentification/models/attention/__init__.py
@@ -1,10 +1,11 @@
+from .block import DynamicalSelfAttentionBlock
 from .embeddings import LinearEmbedding
 from .feedforward import DelayLineFeedforward, TransformerFeedforward
 from .readouts import LinearReadout
 from .self_attention import DynamicalSelfAttention
 
-
 __all__ = [
+    "DynamicalSelfAttentionBlock",
     "DelayLineFeedforward",
     "DynamicalSelfAttention",
     "LinearEmbedding",
