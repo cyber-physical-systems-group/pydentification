c838e4c687aa973b323b8cf902c44454fe6e4216
Krzysztof Zajac <kzajac1997@gmail.com>
2023-09-09 09:06:19 +0200

    [datamodules](feat) Add datamodules package

--- pydentification/data/__init__.py
+++ pydentification/data/__init__.py
@@ -1,6 +1,7 @@
 from .sequences import generate_time_series_windows, time_series_train_test_split
 
 __all__ = [
+    "datamodules",
     "generate_time_series_windows",
     "time_series_train_test_split",
 ]
