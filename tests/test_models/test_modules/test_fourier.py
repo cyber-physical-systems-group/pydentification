f8d2f1a16f4501a1e4408fcf8faea897833d0449
Krzysztof Zajac <kzajac1997@gmail.com>
2023-12-20 11:07:23 +0100

    [models](chore) Move all modules to single package
    BREAKING_CHANGE: Import paths will be deprecated

--- tests/test_models/test_modules/test_fourier.py
+++ tests/test_models/test_modules/test_fourier.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 import torch
 
-from pydentification.models.orthogonal import fourier
+from pydentification.models.modules.orthogonal import fourier
 
 
 @pytest.fixture
