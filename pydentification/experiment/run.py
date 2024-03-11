0ba5c76dcc8c224ec4a1232df9485d7ec203112a
Krzysztof Zajac <kzajac1997@gmail.com>
2024-03-11 09:02:34 +0100

    [experiment](chore) Move entrypoint functions

--- pydentification/experiment/run.py
+++ pydentification/experiment/run.py
@@ -5,13 +5,17 @@ import click
 import wandb
 import yaml
 
-from pydentification.experiment.parameters import left_dict_join
+from .context import RuntimeContext
+from .parameters import left_dict_join
 
 
-def run_training(project_name: str, dataset_config: dict[str, Any], experiment_config: dict[str, Any]) -> None:
+def run_training(
+    context: RuntimeContext, project_name: str, dataset_config: dict[str, Any], experiment_config: dict[str, Any]
+) -> None:
     """
     Runs single training without sweep parameters with all parameters used from static configuration file
 
+    :param context: runtime context
     :param project_name: name of W&B project
     :param dataset_config: static dataset configuration
     :param experiment_config: dynamic experiment configuration
@@ -19,11 +23,11 @@ def run_training(project_name: str, dataset_config: dict[str, Any], experiment_c
     try:
         # some parameters needed for data module are in experiment_config/training, some in experiment_config/model
         data_parameters = left_dict_join(experiment_config["training"], experiment_config["model"])
-        dm = input_fn(dataset_config, data_parameters)  # noqa: F821
-        model, trainer = model_fn(project_name, experiment_config["training"], experiment_config["model"])  # noqa: F821
-        model, trainer = train_fn(model, trainer, dm)  # noqa: F821
-        report_fn(model, trainer, dm)  # noqa: F821
-        save_fn(wandb.run.id, model)  # noqa: F821
+        dm = context.input_fn(dataset_config, data_parameters)
+        model, trainer = context.model_fn(project_name, experiment_config["training"], experiment_config["model"])
+        model, trainer = context.train_fn(model, trainer, dm)
+        context.report_fn(model, trainer, dm)
+        context.save_fn(wandb.run.id, model)
 
     except Exception as e:
         logging.exception(e)  # log traceback, W&B can sometimes lose information
@@ -33,11 +37,11 @@ def run_training(project_name: str, dataset_config: dict[str, Any], experiment_c
 @click.command()
 @click.option("--data", type=click.Path(exists=True), required=True)
 @click.option("--experiment", type=click.Path(exists=True), required=True)
-def main(data: str, experiment: str):
+def main(data: str, experiment: str, context: RuntimeContext):
     dataset_config = yaml.safe_load(open(data))
     experiment_config = yaml.safe_load(open(experiment))
 
     with wandb.init(project=experiment_config["general"]["project"], name=experiment_config["general"]["name"]):
         wandb.log(experiment_config["model"])
         wandb.log(experiment_config["training"])
-        run_training(experiment_config["general"]["project"], dataset_config, experiment_config)
+        run_training(context, experiment_config["general"]["project"], dataset_config, experiment_config)
--- pydentification/experiment/sweep.py
+++ pydentification/experiment/sweep.py
@@ -6,13 +6,17 @@ import click
 import wandb
 import yaml
 
-from pydentification.experiment.parameters import left_dict_join, prepare_config_for_sweep
+from .context import RuntimeContext
+from .parameters import left_dict_join, prepare_config_for_sweep
 
 
-def run_sweep_step(project_name: str, dataset_config: dict[str, Any], experiment_config: dict[str, Any]) -> None:
+def run_sweep_step(
+    context: RuntimeContext, project_name: str, dataset_config: dict[str, Any], experiment_config: dict[str, Any]
+) -> None:
     """
     Runs single experiment in sweep, for more on configuration details see configs/README.md
 
+    :param context: runtime context
     :param project_name: name of W&B project
     :param dataset_config: static dataset configuration
     :param experiment_config: dynamic experiment configuration
@@ -25,11 +29,11 @@ def run_sweep_step(project_name: str, dataset_config: dict[str, Any], experiment
 
         try:
             data_parameters = left_dict_join(experiment_config["training"], experiment_config["model"])  # noqa: F821
-            dm = input_fn(dataset_config, data_parameters)  # noqa: F821
-            model, trainer = model_fn(project_name, experiment_config["training"], parameters)  # noqa: F821
-            model, trainer = train_fn(model, trainer, dm)  # noqa: F821
-            report_fn(model, trainer, dm)  # noqa: F821
-            save_fn(wandb.run.id, model, trainer, dm)  # noqa: F821
+            dm = context.input_fn(dataset_config, data_parameters)
+            model, trainer = context.model_fn(project_name, experiment_config["training"], parameters)
+            model, trainer = context.train_fn(model, trainer, dm)
+            context.report_fn(model, trainer, dm)
+            context.save_fn(wandb.run.id, model)
         except Exception as e:
             logging.exception(e)  # log traceback, W&B can sometimes lose information
             raise ValueError("Experiment failed.") from e
@@ -38,7 +42,7 @@ def run_sweep_step(project_name: str, dataset_config: dict[str, Any], experiment
 @click.command()
 @click.option("--data", type=click.Path(exists=True), required=True)
 @click.option("--experiment", type=click.Path(exists=True), required=True)
-def main(data: str, experiment: str):
+def main(data: str, experiment: str, context: RuntimeContext):
     dataset_config = yaml.safe_load(open(data))
     experiment_config = yaml.safe_load(open(experiment))
 
@@ -46,7 +50,9 @@ def main(data: str, experiment: str):
     sweep_config = prepare_config_for_sweep(experiment_config["sweep"], sweep_parameters)
     sweep_id = wandb.sweep(sweep_config, project=experiment_config["general"]["project"])
 
-    run_sweep_fn = partial(run_sweep_step, experiment_config["general"]["project"], dataset_config, experiment_config)
+    run_sweep_fn = partial(
+        run_sweep_step, context, experiment_config["general"]["project"], dataset_config, experiment_config
+    )
     wandb.agent(
         sweep_id,
         function=run_sweep_fn,
