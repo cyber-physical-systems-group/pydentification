82b6298533e45b3c4f2c88c8715528fd895f1e4d
Krzysztof Zajac <kzajac1997@gmail.com>
2024-05-10 16:39:12 +0200

    [training/callbacks](tests) Rewrite pl.Trainer is tests to be fully mocked.
    BREAKING_CHANGE: The logging of callback properties is done at the end of epoch, so expected values move by 1 epoch back.

--- tests/test_training/test_lightning/mocks.py
+++ tests/test_training/test_lightning/mocks.py
@@ -1,10 +1,9 @@
-from typing import Callable
+from typing import Any, Callable
 
 import lightning.pytorch as pl
 import torch
 from torch import Tensor
 from torch.nn import Module
-from torch.utils.data import Dataset
 
 
 class StepAheadModule(Module):
@@ -16,8 +15,11 @@ class StepAheadModule(Module):
     Gradients are zero all the time, since the output is always constant.
     """
 
-    def __init__(self):
+    def __init__(self, teacher_forcing: bool = False, full_residual_connection: bool = False):
         super(StepAheadModule, self).__init__()
+
+        self.teacher_forcing = teacher_forcing
+        self.full_residual_connection = full_residual_connection
 
         self.parameter = torch.nn.Parameter(torch.rand(1), requires_grad=True)
 
@@ -25,117 +27,76 @@ class StepAheadModule(Module):
         return self.parameter * torch.zeros_like(x[:, 0, :])
 
 
-class RandDataset(Dataset):
+class MockLightningTrainer:
     """
-    Random torch dataset returning given number of elements each with given shape
-    """
+    Mock of Lightning Trainer for testing purposes. It only calls required functions in expected order without
+    implementing the training logic, to allow testing callbacks etc. without long runtime of tests.
 
-    def __init__(self, size: int, shape: tuple):
-        self.size = size
-        self.shape = shape
-
-    def __len__(self) -> int:
-        return self.size
-
-    def __getitem__(self, _) -> Tensor:
-        return torch.rand(self.shape)
-
-
-class ZeroLossPredictionTrainer(pl.LightningModule):
-    """
-    Mock of LightningModule for training n-step-ahead simulation model, which has zero loss all the time.
-    It is used to test training methods and callbacks without need of real loss computation.
+    :note: actual pl.Trainer might have different order of calling callbacks and reloading datamodule and model.
+           the properties changed by callbacks might be off by 1 epoch in assert, depending on when they are logged.
     """
 
     def __init__(
-        self,
-        module: Module,
-        teacher_forcing: bool = False,
-        full_residual_connection: bool = False,
+        self, loss_fn: Callable[[int], Tensor], callbacks: list[pl.Callback], max_epochs: int, *args, **kwargs
     ):
-        super().__init__()
-
-        self.module = module
-
-        self.teacher_forcing = teacher_forcing
-        self.full_residual_connection = full_residual_connection
-
-    def configure_optimizers(self) -> dict:
-        return {"optimizer": torch.optim.Adam(self.module.parameters())}
-
-    def training_step(self, batch: tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
-        # return 0 loss as torch.Variable with gradient to align with required interface
-        return torch.autograd.Variable(torch.Tensor([float(0)]), requires_grad=True)
-
-    def validation_step(self, batch: tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
-        # return 0 loss as torch.Variable with gradient to align with required interface
-        return torch.autograd.Variable(torch.Tensor([float(0)]), requires_grad=True)
-
-    def predict_step(self, batch: tuple[Tensor, Tensor]):
-        x, y = batch
-        return self.module(x)
-
-    def on_train_epoch_end(self) -> None:
-        self.log_dict({f"teacher_forcing_at_{self.current_epoch}": self.teacher_forcing}, on_epoch=True, on_step=False)
-        self.log_dict(
-            {f"full_residual_connection_at_{self.current_epoch}": self.full_residual_connection},
-            on_epoch=True,
-            on_step=False,
-        )
-
-
-class FunctionLossPredictionTrainer(pl.LightningModule):
-    """
-    Mock of LightningModule for training n-step-ahead simulation model, which returns loss a any function of epoch.
-    It is used to test training methods and callbacks without need of real loss computation.
-    """
-
-    def __init__(
-        self,
-        module: Module,
-        loss_fn: Callable[[int], Tensor],
-        teacher_forcing: bool = False,
-        full_residual_connection: bool = False,
-    ):
-        super().__init__()
-
-        self.module = module
+        """
+        :param loss_fn: function returning loss value for given epoch
+        :param callbacks: list of callbacks to be used during training
+        :param max_epochs: number of epochs to train
+        """
         self.loss_fn = loss_fn
+        self.max_epochs = max_epochs
+        self.callbacks = callbacks
+        # placeholders
+        self.model = None
+        self.datamodule = None
+        self.optimizers = None
+        # running variables
+        self.current_epoch = 0
+        self.callback_metrics = {}
+        self.logged_metrics = []
 
-        self.teacher_forcing = teacher_forcing
-        self.full_residual_connection = full_residual_connection
+    def store(self, datamodule: pl.LightningDataModule, model: pl.LightningModule) -> dict[str, Any]:
+        """Store properties modified by callbacks to check them in tests"""
+        time_steps = datamodule.n_forward_time_steps
+        epoch_metrics = {"n_forward_time_steps": time_steps, "teacher_forcing": model.teacher_forcing}
 
-    def configure_optimizers(self) -> dict:
-        # initialize optimizer with learning rate 1 to make tests easier to read
-        return {"optimizer": torch.optim.Adam(self.module.parameters(), lr=1)}
+        for index, optimizer in enumerate(self.optimizers):
+            for group_index, param_group in enumerate(optimizer.param_groups):
+                # log learning rate for each epoch for each optimizer and its parameter groups
+                epoch_metrics[f"learning_rate_optimizer_{index}_{group_index}"] = param_group["lr"]
 
-    def training_step(self, batch: tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
-        loss_value = self.loss_fn(self.current_epoch)
-        loss = torch.autograd.Variable(torch.Tensor([loss_value]), requires_grad=True)
-        self.log("train_loss", loss)
-        return loss
+        return epoch_metrics
 
-    def validation_step(self, batch: tuple[Tensor, Tensor], batch_idx: int) -> Tensor:
-        loss_value = self.loss_fn(self.current_epoch)
-        loss = torch.autograd.Variable(torch.Tensor([loss_value]), requires_grad=True)
-        self.log("val_loss", loss)
-        return loss
-
-    def predict_step(self, batch: tuple[Tensor, Tensor]):
-        x, y = batch
-        return self.module(x)
-
-    def on_train_epoch_end(self) -> None:
+    def fit(self, model: pl.LightningModule, datamodule: pl.LightningDataModule):
         """
-        Log attributes given in __init__ to the logger after each epoch.
-        Each key is the epoch number and value is dict of attributes and their values after the epoch.
+        Mocked fit method, calling callbacks in expected order and storing metrics for testing.
+        note: this does not run actual training or Any Tensor computations, only calls callbacks in expected order.
         """
-        time_steps = self.trainer.datamodule.n_forward_time_steps
+        # store model, optimizers and datamodule for callbacks
+        self.model = model
+        self.optimizers = [torch.optim.Adam(model.parameters(), lr=1)]
+        self.datamodule = datamodule
 
-        self.log_dict({f"n_forward_time_steps_at_{self.current_epoch}": time_steps}, on_epoch=True, on_step=False)
-        self.log_dict({f"teacher_forcing_at_{self.current_epoch}": self.teacher_forcing}, on_epoch=True, on_step=False)
+        for callback in self.callbacks:
+            callback.on_train_start(self, self.model)
 
-        # log learning rate for each epoch for each optimizer and its parameter groups
-        for i, optimizer in enumerate(self.trainer.optimizers):
-            for param_group in optimizer.param_groups:
-                self.log_dict({f"learning_rate_at_{self.current_epoch}_optimizer_{i}": param_group["lr"]})
+        for epoch in range(self.max_epochs):
+            self.current_epoch = epoch  # set current epoch for callbacks
+
+            for callback in self.callbacks:
+                callback.on_train_epoch_start(self, self.model)
+                callback.on_validation_epoch_start(self, self.model)
+
+            loss_value = self.loss_fn(self.current_epoch)
+            loss = torch.autograd.Variable(torch.Tensor([loss_value]), requires_grad=True)
+            self.callback_metrics["val_loss"] = loss
+
+            for callback in self.callbacks:
+                callback.on_train_epoch_end(self, self.model)
+                callback.on_validation_epoch_end(self, self.model)
+
+            self.logged_metrics.append(self.store(self.datamodule, self.model))
+
+        for callback in self.callbacks:
+            callback.on_train_end(self, self.model)
--- tests/test_training/test_lightning/test_callbacks.py
+++ tests/test_training/test_lightning/test_callbacks.py
@@ -1,12 +1,10 @@
 from typing import Literal
 
-import lightning.pytorch as pl
 import pytest
-from torch.utils.data import DataLoader
 
 from pydentification.training.lightning import callbacks
 
-from .mocks import FunctionLossPredictionTrainer, RandDataset, StepAheadModule, ZeroLossPredictionTrainer
+from .mocks import MockLightningTrainer, StepAheadModule
 
 
 @pytest.mark.parametrize(
@@ -22,39 +20,35 @@ from .mocks import FunctionLossPredictionTrainer, RandDataset, StepAheadModule, 
         (5, 5, [True, True, True, True, True]),
     ),
 )
-def test_cyclic_teacher_forcing(cycle_in_epochs: int, max_epochs: int, expected: list[bool]):
-    trainable_module = ZeroLossPredictionTrainer(module=StepAheadModule(), teacher_forcing=True)
-    dataloader = DataLoader(RandDataset(size=10, shape=(10, 1)), batch_size=1, shuffle=False)
-
-    trainer = pl.Trainer(
-        accelerator="cpu",  # only CPU for unit-tests
+def test_cyclic_teacher_forcing(
+    cycle_in_epochs: int, max_epochs: int, expected: list[bool], random_prediction_datamodule
+):
+    trainer = MockLightningTrainer(
+        loss_fn=lambda x: float(0),  # mock loss function always returning zero-loss
         max_epochs=max_epochs,
         callbacks=[callbacks.CyclicTeacherForcing(cycle_in_epochs=cycle_in_epochs)],
     )
 
-    trainer.fit(trainable_module, dataloader)
-    # each epoch teacher forcing is logged with key to distinguish epochs
-    metrics = trainer.logged_metrics.items()
-    teacher_forcing_history = {key: bool(value) for key, value in metrics if key.startswith("teacher_forcing")}
+    random_prediction_datamodule.n_forward_time_steps = 1  # set to 1 before fit, the datamodule is shared between tests
+    trainer.fit(StepAheadModule(teacher_forcing=True), datamodule=random_prediction_datamodule)
 
-    assert list(teacher_forcing_history.values()) == expected
+    assert [epoch["teacher_forcing"] for epoch in trainer.logged_metrics] == expected
 
 
 @pytest.mark.parametrize(
     ["mock_loss_fn", "max_epochs", "patience", "factor", "max_length", "expected_lengths"],
     (
         # constant loss so trainer sees no improvement
-        # 5 epoch patience increases the length by 2 at 5 epoch and again at 10 epoch
-        # length is logged for each epoch at the end, so after changing with the callback
-        (lambda x: float(0), 10, 5, 2, float("inf"), [1, 1, 1, 1, 2, 2, 2, 2, 2, 4]),
+        # 5 epoch patience increases the length by 2 at 6 epoch
+        (lambda x: float(0), 10, 5, 2, float("inf"), [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]),
         # linearly decreasing loss, so trainer sees improvement and keeps the length
         (lambda x: float(10 - x), 10, 5, 2, float("inf"), [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
         # linearly increasing loss, length is increased every epoch (patience is 1)
-        (lambda x: float(x), 10, 1, 2, float("inf"), [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]),
+        (lambda x: float(x), 10, 1, 2, float("inf"), [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]),
         # loss is dropping every second epoch
-        (lambda x: 10 - x - (x % 2), 10, 1, 2, float("inf"), [2, 2, 4, 4, 8, 8, 16, 16, 32, 32]),
-        # max length is reached in 5 epoch
-        (lambda x: float(x), 10, 1, 2, 32, [2, 4, 8, 16, 32, 32, 32, 32, 32, 32]),
+        (lambda x: 10 - x - (x % 2), 10, 1, 2, float("inf"), [1, 1, 2, 2, 4, 4, 8, 8, 16, 16]),
+        # max length is reached in 6 epoch
+        (lambda x: float(x), 10, 1, 2, 32, [1, 2, 4, 8, 16, 32, 32, 32, 32, 32]),
     ),
 )
 def test_increase_autoregression_length_on_plateau(
@@ -66,7 +60,6 @@ def test_increase_autoregression_length_on_plateau(
     expected_lengths: list[int],
     random_prediction_datamodule,
 ):
-    trainable_module = FunctionLossPredictionTrainer(StepAheadModule(), loss_fn=mock_loss_fn)
     callback = callbacks.IncreaseAutoRegressionLengthOnPlateau(
         monitor="val_loss",
         patience=patience,
@@ -75,19 +68,16 @@ def test_increase_autoregression_length_on_plateau(
         verbose=True,
     )
 
-    trainer = pl.Trainer(
-        accelerator="cpu",
+    trainer = MockLightningTrainer(
+        loss_fn=mock_loss_fn,
         max_epochs=max_epochs,
-        reload_dataloaders_every_n_epochs=1,
         callbacks=[callback],
     )
 
     random_prediction_datamodule.n_forward_time_steps = 1  # set to 1 before fit, the datamodule is shared between tests
-    trainer.fit(trainable_module, datamodule=random_prediction_datamodule)
+    trainer.fit(StepAheadModule(), datamodule=random_prediction_datamodule)
 
-    metrics = trainer.logged_metrics.items()
-    ar_length_history = [int(value) for key, value in metrics if key.startswith("n_forward_time_steps")]
-    assert ar_length_history == expected_lengths
+    assert [epoch["n_forward_time_steps"] for epoch in trainer.logged_metrics] == expected_lengths
 
 
 @pytest.mark.parametrize(
@@ -100,71 +90,67 @@ def test_increase_autoregression_length_on_plateau(
     ],
     (
         (
-            # two cycles in the callback - one in 5 epoch and one in 10 epoch, loss does not change and patience is 5
-            # first cycle increases the length by 2, second cycle decreases the learning rate to 0.1
+            # two cycles in the callback - every second epoch auto-regression length or learning rate is changed
             ["ar_length", "learning_rate"],
             False,  # reset_learning_rate
-            # auto-regression length is increased by 2 at 5 epoch
-            [1, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8, 8],
-            [1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.01, 0.01, 0.01, 0.01, 0.001],
-            [True] * 12,  # teacher forcing is not changed, since it is not in cycles
+            [1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8],
+            [1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1, 0.01, 0.01, 0.01, 0.01],
+            [True] * 12,  # teacher forcing is not changed (not given in cycles)
         ),
         (
-            ["ar_length", "learning_rate"],
-            True,  # reset_learning_rate
-            # callback is called each second epoch
-            # auto-regression length is increased at 2nd, 6th and 10th epoch
-            [1, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8, 8],
-            # learning rate is reset at the end of each cycle, so it does not change
-            [float(1)] * 12,
-            [True] * 12,  # teacher forcing is not changed, since it is not in cycles
-        ),
-        (
+            # two cycles in the callback - every second epoch auto-regression length or learning rate is changed
+            # order is inverted with respect to the previous test, everything else should be the same
             ["learning_rate", "ar_length"],
             True,  # reset_learning_rate
-            # callback is called each second epoch
-            # auto-regression length is increased at 2nd, 6th and 10th epoch
-            [1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 8],
-            # learning rate is reset at the end of each cycle, so it does not change
-            [1, 0.1, 0.1, 1, 1, 0.1, 0.1, 1, 1, 0.1, 0.1, 1],
-            [True] * 12,  # teacher forcing is not changed, since it is not in cycles
+            [1, 1, 1, 1, 2, 2, 2, 2, 4, 4, 4, 4],
+            [1, 1, 0.1, 0.1, 1, 1, 0.1, 0.1, 1, 1, 0.1, 0.1],
+            [True] * 12,  # teacher forcing is not changed (not given in cycles)
         ),
         (
+            # callback is called each second epoch, but learning rate is reset at the end of each cycle,
+            # so it does not change
+            ["ar_length", "learning_rate"],
+            True,  # reset_learning_rate
+            [1, 1, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8],
+            [float(1)] * 12,  # constant learning rate
+            [True] * 12,  # teacher forcing is not changed (not given in cycles)
+        ),
+        (
+            # learning rate is changed 3 times, before changing auto-regression length
             ["learning_rate", "learning_rate", "learning_rate", "ar_length"],
             True,  # reset_learning_rate
-            # auto-regression length is increased at 8th epoch
-            [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
-            # learning rate is decreases every second epoch and is reset at epoch 8 - at the end of the cycle
-            [1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 1, 1, 0.1, 0.1, 0.01],
-            [True] * 12,  # teacher forcing is not changed, since it is not in cycles
+            [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2],
+            [1, 1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 1, 1, 0.1, 0.1],
+            [True] * 12,  # teacher forcing is not changed (not given in cycles)
         ),
         (
+            # learning rate is changed 3 times, before changing auto-regression length 2 times
             ["learning_rate", "learning_rate", "learning_rate", "ar_length", "ar_length"],
             True,  # reset_learning_rate
-            # auto-regression length is increased at 8th and 10-th epoch
-            [1, 1, 1, 1, 1, 1, 1, 2, 2, 4, 4, 4],
+            # auto-regression length is increased at 9-th and 11-th epoch (after learning rate is changed 3 times)
+            [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 4, 4],
             # learning rate is decreases until epoch 8 when cycle goes to increasing AR length twice
-            # it is reset at epoch 10 - at the end of the cycle
-            [1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 0.001, 0.001, 1, 1, 0.1],
-            [True] * 12,  # teacher forcing is not changed, since it is not in cycles
+            # it is reset at epoch 11 - at the end of the cycle
+            [1, 1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 0.001, 0.001, 1, 1],
+            [True] * 12,  # teacher forcing is not changed (not given in cycles)
         ),
         (
+            # teacher forcing toggle is added as the third cycle
             ["learning_rate", "ar_length", "teacher_forcing"],
             False,  # reset_learning_rate
-            # auto-regression length is increased at 8th and 10-th epoch
-            [1, 1, 1, 2, 2, 2, 2, 2, 2, 4, 4, 4],
-            # learning rate is decreases until epoch 8 when cycle goes to increasing AR length twice
-            # it is reset at epoch 10 - at the end of the cycle
-            [1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.01, 0.01, 0.01, 0.01, 0.01],
-            # teacher forcing is changed at 6th and 12th epoch
-            [True, True, True, True, True, False, False, False, False, False, False, True],
+            [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 4, 4],
+            [1, 1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.01, 0.01, 0.01, 0.01],
+            # teacher forcing is changed at 7-th epoch
+            [True, True, True, True, True, True, False, False, False, False, False, False],
         ),
         (
+            # learning rate is changed 3 times, before changing auto-regression length and toggling teacher forcing
             ["learning_rate", "learning_rate", "learning_rate", "ar_length", "teacher_forcing"],
             True,  # reset_learning_rate
-            [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
-            [1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 0.001, 0.001, 1, 1, 0.1],
-            [True, True, True, True, True, True, True, True, True, False, False, False],
+            [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2],
+            # learning rate is reset to initial value at epoch 11, which is the end of the cycle
+            [1, 1, 0.1, 0.1, 0.01, 0.01, 0.001, 0.001, 0.001, 0.001, 1, 1],
+            [True, True, True, True, True, True, True, True, True, True, False, False],
         ),
     ),
 )
@@ -176,12 +162,6 @@ def test_combined_autoregression_callback_cycles(
     expected_teacher_forcing: list[bool],
     random_prediction_datamodule,
 ):
-    trainable_module = FunctionLossPredictionTrainer(
-        StepAheadModule(),
-        loss_fn=lambda x: float(0),
-        teacher_forcing=True,
-    )
-
     callback = callbacks.CombinedAutoRegressionCallback(
         cycles=cycles,
         monitor="val_loss",
@@ -193,21 +173,17 @@ def test_combined_autoregression_callback_cycles(
         verbose=True,
     )
 
-    trainer = pl.Trainer(
-        accelerator="cpu",
+    trainer = MockLightningTrainer(
+        loss_fn=lambda x: float(0),
         max_epochs=12,
-        reload_dataloaders_every_n_epochs=1,
         callbacks=[callback],
     )
 
     random_prediction_datamodule.n_forward_time_steps = 1  # set to 1 before fit, the datamodule is shared between tests
-    trainer.fit(trainable_module, datamodule=random_prediction_datamodule)
+    trainer.fit(StepAheadModule(teacher_forcing=True), datamodule=random_prediction_datamodule)
 
-    metrics = trainer.logged_metrics.items()
-    ar_length_history = [int(value) for key, value in metrics if key.startswith("n_forward_time_steps")]
-    learning_rate_history = [pytest.approx(float(value)) for key, value in metrics if key.startswith("learning_rate")]
-    teacher_forcing_history = [bool(value) for key, value in metrics if key.startswith("teacher_forcing")]
-
-    assert ar_length_history == expected_lengths
-    assert learning_rate_history == expected_learning_rates
-    assert teacher_forcing_history == expected_teacher_forcing
+    # only single optimizer with single parameter group is used in the test
+    learning_rates = [pytest.approx(epoch["learning_rate_optimizer_0_0"]) for epoch in trainer.logged_metrics]
+    assert learning_rates == expected_learning_rates
+    assert [epoch["n_forward_time_steps"] for epoch in trainer.logged_metrics] == expected_lengths
+    assert [epoch["teacher_forcing"] for epoch in trainer.logged_metrics] == expected_teacher_forcing
