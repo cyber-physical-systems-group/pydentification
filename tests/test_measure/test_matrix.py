84b92e78577c62c552a66a0bda629668650734c7
Krzysztof Zajac <kzajac1997@gmail.com>
2024-08-14 14:49:41 +0200

    [measure](test) Add approximate unitarity tests

--- tests/test_measure/test_matrix.py
+++ tests/test_measure/test_matrix.py
@@ -22,3 +22,24 @@ from tests.test_measure import utils
 )
 def test_measure_exact_orthogonality(matrix: torch.Tensor):
     assert unitarity(matrix) == float(1)
+
+
+@pytest.mark.parametrize(
+    ["randomness", "expected_low", "expected_high"],
+    (
+        # see description of measure statistics in pydentification/measure/README.md
+        (0.1, 0.82, 0.88),
+        (0.2, 0.62, 0.72),
+        (0.3, 0.40, 0.56),
+        (0.4, 0.22, 0.35),
+        (0.5, 0.10, 0.22),
+        (0.6, 0.04, 0.12),
+        (0.7, 0, 0.5),
+        (0.8, 0, 0.02),
+        (0.9, 0, 0.01),
+        (1.0, 0, 0.005),
+    ),
+)
+def test_measure_approximate_orthogonality(randomness: float, expected_low: float, expected_high: float):
+    matrix = torch.eye(32) + randomness * torch.randn(32, 32)
+    assert expected_low <= unitarity(matrix) <= expected_high
