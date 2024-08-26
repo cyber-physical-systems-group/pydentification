bcde93a92fa824393fe15b935e471bdb2204cd9c
Krzysztof Zajac <kzajac1997@gmail.com>
2024-08-26 21:28:09 +0200

    [models](test) Add reference test for non-causal module

--- tests/test_models/test_transformer/test_causality.py
+++ tests/test_models/test_transformer/test_causality.py
@@ -157,3 +157,41 @@ def test_masked_self_attention_causality(kwargs: dict[str, Any], shape: tuple, c
         assert torch.allclose(reference[:, :change_step], changed[:, :change_step])
         # this partly relies on initialization, if weight for some connection is close to zero (random seed used)
         assert not torch.allclose(reference[:, change_step:], changed[:, change_step:])
+
+
+@pytest.mark.parametrize(
+    ["kwargs", "shape", "change_step"],
+    (
+        # batch_size = 1, n_time_steps = 10, n_state_variables = 1
+        ({"n_time_steps": 10, "n_state_variables": 1}, (1, 10, 1), 5),
+        ({"n_time_steps": 10, "n_state_variables": 1, "bias": False}, (1, 10, 1), 5),  # with bias
+        ({"n_time_steps": 10, "n_state_variables": 1, "skip_connection": True}, (1, 10, 1), 5),  # with skip connection
+        ({"n_time_steps": 10, "n_state_variables": 1}, (5, 10, 1), 5),  # with higher batch size
+        # batch_size = 1, n_time_steps = 10, n_state_variables = 5
+        ({"n_time_steps": 10, "n_state_variables": 5}, (1, 10, 5), 5),
+        ({"n_time_steps": 10, "n_state_variables": 5, "bias": False}, (1, 10, 5), 5),  # with bias
+        ({"n_time_steps": 10, "n_state_variables": 5, "skip_connection": True}, (1, 10, 5), 5),  # with skip connection
+        ({"n_time_steps": 10, "n_state_variables": 5}, (5, 10, 5), 5),  # with higher batch size
+        # batch_size = 1, n_time_steps = 10, n_state_variables = 5
+        # change input early
+        ({"n_time_steps": 10, "n_state_variables": 1}, (1, 10, 1), 1),
+        ({"n_time_steps": 10, "n_state_variables": 1, "bias": False}, (1, 10, 1), 1),  # with bias
+        ({"n_time_steps": 10, "n_state_variables": 1, "skip_connection": True}, (1, 10, 1), 1),  # with skip connection
+        ({"n_time_steps": 10, "n_state_variables": 1}, (5, 10, 1), 1),  # with higher batch size
+    ),
+)
+def test_delay_line_causality(kwargs: dict[str, Any], shape: tuple, change_step: int):
+    """Reference test for unmasked model, which is not causal"""
+    torch.manual_seed(42)  # needed for second assert
+    module = transformer.DelayLineFeedforward(**kwargs)
+
+    with torch.no_grad():
+        reference = module(torch.ones(shape))  # type: ignore
+        # change at given time step to two
+        changed = module(change_time_series_signal(torch.ones(shape), change_step=change_step, value=2.0))
+
+        # check that the change is only at the given time step
+        assert not torch.allclose(reference[:, :change_step], changed[:, :change_step])
+        # this partly relies on initialization, if weight for some connection is close to zero (random seed used)
+        assert not torch.allclose(reference[:, change_step:], changed[:, change_step:])
+
