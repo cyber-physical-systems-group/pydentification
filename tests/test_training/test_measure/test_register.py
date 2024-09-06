4b8fef2344964935007714ccd316d355f191f369
Krzysztof Zajac <kzajac1997@gmail.com>
2024-09-06 16:45:51 +0200

    [measure/callbacks](tests) Add refined unittest for measure register for square matrix parameters

--- tests/test_training/test_measure/test_register.py
+++ tests/test_training/test_measure/test_register.py
@@ -9,6 +9,7 @@ from pydentification.training.measure import register
     ["model", "register_fn", "expected_names"],
     [
         # Test cases for register_all_parameters
+
         # Linear layer has two parameters (weight, bias) -> register both
         [torch.nn.Linear(10, 10), register.register_all_parameters, ["weight", "bias"]],
         # Conv2d layer has four parameters (weight, bias) -> register all
@@ -31,7 +32,8 @@ from pydentification.training.measure import register
             register.register_all_parameters,
             ["weight_ih_l0", "weight_hh_l0", "bias_ih_l0", "bias_hh_l0"],
         ],
-        # LSTM layer has eight parameters (weight_ih_l0, weight_hh_l0, bias_ih_l0, bias_hh_l0, weight_ih_l0_reverse, weight_hh_l0_reverse, bias_ih_l0_reverse, bias_hh_l0_reverse) -> register all
+        # LSTM layer has eight parameters (weight_ih_l0, weight_hh_l0, bias_ih_l0, bias_hh_l0, weight_ih_l0_reverse,
+        # weight_hh_l0_reverse, bias_ih_l0_reverse, bias_hh_l0_reverse) -> register all
         [
             torch.nn.LSTM(10, 10, 1),
             register.register_all_parameters,
@@ -172,7 +174,52 @@ from pydentification.training.measure import register
                 "decoder.layers.1.linear2.weight",
             ],
         ],
+        # Test cases for register_square_parameters
+
+        # Linear layer has one matrix (weight) and one vector (bias) parameter -> register only weight
+        [torch.nn.Linear(10, 10), register.register_square_parameters, ["weight"]],
+        # Conv2d layer parameters has shape (out_channels, in_channels, kernel_size, kernel_size) -> 4D is not matrix
+        [torch.nn.Conv2d(3, 3, 3), register.register_square_parameters, []],  # noqa
+        # Sequential model has two Linear layers -> register both weights
+        [
+            torch.nn.Sequential(torch.nn.Linear(10, 10), torch.nn.Linear(10, 10)),
+            register.register_square_parameters,
+            ["0.weight", "1.weight"],
+        ],
+        # Sequential model with Conv2d and activation -> register only Conv2d weight
+        [
+            torch.nn.Sequential(torch.nn.Linear(10, 10), torch.nn.ReLU()),
+            register.register_square_parameters,
+            ["0.weight"],
+        ],
+        # RNN layer has three parameters (weight_ih_l0, weight_hh_l0, bias_ih_l0) -> register only weights
+        [torch.nn.RNN(10, 10, 1), register.register_square_parameters, ["weight_ih_l0", "weight_hh_l0"]],
+        # Parameters of LSTM layer are stored in one matrix, but it is not square matrix -> return empty list
+        [torch.nn.LSTM(10, 10, 1), register.register_square_parameters, []],
+        # Sequential composed of Linear, batch-norm and activation -> register only Linear weight
+        [
+            torch.nn.Sequential(torch.nn.Linear(10, 10), torch.nn.BatchNorm1d(10), torch.nn.ReLU()),
+            register.register_square_parameters,
+            ["0.weight"],
+        ],
+        # Small 2-layer encoder-decoder transformer will have 6 square matrix parameters -> register all
+        # weights for Q,K and V transformation are stored in one parameter (`in_proj_weight`), but it is not square
+        # feed-forward modules have non-square up and down projections, which are not square matrices
+        # all other parameters are not square matrices or vector parameters
+        [
+            torch.nn.Transformer(nhead=2, num_encoder_layers=2, num_decoder_layers=2),
+            register.register_square_parameters,
+            [
+                "encoder.layers.0.self_attn.out_proj.weight",
+                "encoder.layers.1.self_attn.out_proj.weight",
+                "decoder.layers.0.self_attn.out_proj.weight",
+                "decoder.layers.0.multihead_attn.out_proj.weight",
+                "decoder.layers.1.self_attn.out_proj.weight",
+                "decoder.layers.1.multihead_attn.out_proj.weight",
+            ],
+        ],
     ],
+
 )
 def test_register_matrix_parameters(model: Module, register_fn: register.RegisterCallable, expected_names: list[str]):
     names = [name for name, _ in register_fn(model)]
