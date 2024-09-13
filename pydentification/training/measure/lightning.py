d9892b9fc189cd90cca1e656a040d7cfa33c5324
Krzysztof Zajac <kzajac1997@gmail.com>
2024-09-13 15:50:14 +0200

    [measure](feat) Simplify LightningMeasure

--- pydentification/training/measure/lightning.py
+++ pydentification/training/measure/lightning.py
@@ -7,7 +7,6 @@ from torch.nn import Module, Parameter
 
 from .register import iter_modules_and_parameters
 
-
 # callable registering parameters of neural network for given measure
 # input will be entire torch.nn.Module and output is list of names (submodule combined with parameter)
 # which will be measured by given measuring function
@@ -16,12 +15,12 @@ RegisterCallable = Callable[[Module], Iterator[tuple[str, Parameter]]]
 MeasureCallable = Callable[[Parameter], float | Tensor]
 # callable processing measured value, input is measured value and output is processed value
 # returns float or Tensor or dictionary of values, such as statistics or multiple measures (or Tensor measure)
-ProcessCallable = Callable[[float | Tensor], float | Tensor | dict[str, float | Tensor]]
+PostProcessCallable = Callable[[float | Tensor], float | Tensor | dict[str, float | Tensor]]
 
 
 @dataclass
 class Measure:
-    measure_name: str
+    name: str
     parameter_name: str
     value: float | Tensor
     representation: float | dict[str, float] | None
@@ -44,19 +43,19 @@ class LightningMeasure:
         name: str,
         measure_fn: MeasureCallable,
         register_fn: RegisterCallable = iter_modules_and_parameters,  # default to registering all parameters
-        process_fn: ProcessCallable | None = None,  # default to no processing
+        postprocess_fn: PostProcessCallable | None = None,  # default to no processing
     ):
         """
         :param name: name of the measure, will be returned for each call
         :param measure_fn: callable measuring single parameter of neural network
         :param register_fn: callable registering parameters of neural network for given measure
-        :param process_fn: callable processing measured value, returns processed value or dictionary of values
+        :param postprocess_fn: callable processing measured value, returns processed value or dictionary of values
         """
         self.name = name
 
         self.measure = measure_fn
         self.register_fn = register_fn
-        self.process_fn = process_fn
+        self.postprocess_fn = postprocess_fn
 
         self.register: set[str] | None = None
 
@@ -68,7 +67,7 @@ class LightningMeasure:
         for name, parameter in iter_modules_and_parameters(module):
             if name in set(self.register):
                 value = self.measure(parameter)
-                if self.process_fn:  # add post-processed representation
-                    yield Measure(self.name, name, value, self.process_fn(value))
+                if self.postprocess_fn:  # add post-processed representation
+                    yield Measure(self.name, name, value, self.postprocess_fn(value))
 
                 yield Measure(self.name, name, value, None)
