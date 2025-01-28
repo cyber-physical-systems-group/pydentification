2f79a5b85ad24776360e43351df172f3e9500a67
Krzysztof Zajac <kzajac1997@gmail.com>
2025-01-28 19:22:33 +0100

    [experiment](chore) Add code for saving only torch.nn.Module

--- pydentification/experiment/defaults/save.py
+++ pydentification/experiment/defaults/save.py
@@ -1,11 +1,14 @@
 import os
 
+import lightning.pytorch as pl
 import torch
 import wandb
 
 
-def save_fn(name: str, model: torch.nn.Module):
+def save_torch_module(name: str, model: pl.LightningModule):
+    """Saves the torch model from given LightningModule to W&B"""
     path = f"models/{name}/trained-model.pt"
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    torch.save(model, path)
+
+    torch.save(model.module.state_dict(), path)  # saves only torch
     wandb.save(path)
