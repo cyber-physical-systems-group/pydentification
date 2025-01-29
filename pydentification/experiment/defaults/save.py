045880fd76f91e1479d4c20cd6e9d1995cc5bced
Krzysztof Zajac <kzajac1997@gmail.com>
2025-01-29 21:34:54 +0100

    [experiment](feat) Add loading with safetensors

--- pydentification/experiment/defaults/save.py
+++ pydentification/experiment/defaults/save.py
@@ -1,14 +1,22 @@
 import os
+from typing import Literal
 
 import lightning.pytorch as pl
 import torch
 import wandb
+from safetensors.torch import save_model
 
 
-def save_torch_module(name: str, model: pl.LightningModule):
+def save_torch_module(name: str, model: pl.LightningModule, method: Literal["pt", "safetensors"] = "safetensors"):
     """Saves the torch model from given LightningModule to W&B"""
-    path = f"models/{name}/trained-model.pt"
+    path = f"models/{name}/trained-model.{method}"
     os.makedirs(os.path.dirname(path), exist_ok=True)
 
-    torch.save(model.module.state_dict(), path)  # saves only torch
+    if method == "safetensors":
+        save_model(model.module, path)
+    elif method == "pt":
+        torch.save(model.module.state_dict(), path)  # saves only torch
+    else:
+        raise ValueError(f"Unknown method: {method}!")
+
     wandb.save(path)
