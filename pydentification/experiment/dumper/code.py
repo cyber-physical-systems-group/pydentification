8bbfb3c0c241e39cc1d2503b5d68e12ea17fa4be
Krzysztof Zajac <kzajac1997@gmail.com>
2025-02-02 13:16:21 +0100

    [experiment](chore) Simplify exclusion logic

--- pydentification/experiment/dumper/code.py
+++ pydentification/experiment/dumper/code.py
@@ -4,10 +4,10 @@ import uuid
 from pathlib import Path
 
 PYTHON_EXTENSIONS = frozenset({".py", ".json", ".txt", ".md", ".yaml", ".yml", ".toml", ".ini"})
-FORBIDDEN_PREFIX = frozenset({"venv", ".ipynb_checkpoints", "__pycache__", ".git", ".pytest_cache"})
+DEFAULT_FORBIDDEN_PREFIX = frozenset({"venv", ".ipynb_checkpoints", "__pycache__", ".git", ".pytest_cache"})
 
 
-def load_gitignore() -> set[str]:
+def _load_gitignore() -> set[str]:
     """Load .gitignore from default name and root directory as set"""
 
     def not_comment(line: str) -> bool:
@@ -21,8 +21,22 @@ def load_gitignore() -> set[str]:
         return set(filter(not_comment, f.read().splitlines()))
 
 
+def _skip_subdir(current: Path, archive_path: Path, forbidden_paths: frozenset[str]) -> bool:
+    # prevent copying the temp directory, where the archive with source code is build
+    if str(archive_path.absolute()) == current:
+        return True
+    # prevent copying the parent directory of the temp directory
+    elif archive_path in current.parents:
+        return True
+    # prevent copying the forbidden paths from defaults and .gitignore
+    elif any(part.startswith(prefix) for prefix in forbidden_paths for part in current.parts):
+        return True
+    return False
+
+
 def save_code_snapshot(name: str, source_dir: str | Path):
     """Save only text-based files in a ZIP archive, excluding binary data files."""
+
     if isinstance(source_dir, str):
         source_dir = Path(source_dir)
 
@@ -30,7 +44,8 @@ def save_code_snapshot(name: str, source_dir: str | Path):
     snapshot_filename = f"source_code_{name}"
     temp_dir = Path(f"temp_code_snapshot_{uuid.uuid4()}")  # append random UUID to avoid conflicts
 
-    forbidden = FORBIDDEN_PREFIX | load_gitignore()  # union of forbidden prefixes and gitignore
+    gitignore = _load_gitignore()
+    forbidden = DEFAULT_FORBIDDEN_PREFIX | gitignore
 
     if temp_dir.exists():
         shutil.rmtree(temp_dir)
@@ -39,11 +54,7 @@ def save_code_snapshot(name: str, source_dir: str | Path):
 
     for root, dirs, files in os.walk(source_dir):
         root_path = Path(root)
-        if (
-            str(temp_dir.absolute()) == root  # prevent copying the temp directory
-            or temp_dir in root_path.parents
-            or any(part.startswith(prefix) for prefix in forbidden for part in root_path.parts)
-        ):
+        if _skip_subdir(root_path, temp_dir, forbidden):
             dirs.clear()  # prevent descending into this directory
             continue  # skip to the next directory
 
