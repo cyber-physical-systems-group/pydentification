e72d240503025da24bd017970b0bfb6f865ef071
Krzysztof Zajac <kzajac1997@gmail.com>
2025-02-04 10:40:13 +0100

    [experiment](feat) Add variable replacement in function source code

--- pydentification/experiment/dumper/parsing.py
+++ pydentification/experiment/dumper/parsing.py
@@ -1,8 +1,39 @@
 import ast
+import re
 import textwrap
+from typing import Any
 
 import black
 import isort
+
+
+def replace_variables(code: str, variables: dict[str, Any]) -> str:
+    """
+    Function parsing source code of some python function as string and replacing variables from dictionary with their
+    values, given as dictionary. This is used to create self-contained code snippets for constructing torch models.
+
+    :note: Variables are only replaced in the function body.
+    """
+    tree = ast.parse(code)
+    function = next((node for node in tree.body if isinstance(node, ast.FunctionDef)), None)
+    pattern = r"\b(" + "|".join(re.escape(var) for var in variables.keys()) + r")\b"
+
+    if not function:
+        raise ValueError("No function definition found in the code.")
+
+    def replacer(matched: re.Match) -> str:
+        var_name = matched.group(0)
+        value = variables[var_name]  # replace name with corresponding value
+        return repr(value) if isinstance(value, str) else str(value)
+
+    # find function start position
+    func_start = function.body[0].lineno - 1  # adjust line numbers
+    # split code into lines and process only the function body
+    lines = code.splitlines()
+    function_body_lines = lines[func_start:]
+    replaced_body = [re.sub(pattern, replacer, line) for line in function_body_lines]
+
+    return "\n".join(lines[:func_start] + replaced_body)  # join lines back together
 
 
 def remove_decorators(code: str, names: set[str]) -> str:
