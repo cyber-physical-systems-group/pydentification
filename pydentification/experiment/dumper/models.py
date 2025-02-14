63b017758123a5df0ae0a0579c5315d75ad4c938
Krzysztof Zajac <kzajac1997@gmail.com>
2025-02-14 14:42:51 +0100

    [experiment](fix) Move responsibility to save to W&B outside model saving functions

--- pydentification/experiment/dumper/models.py
+++ pydentification/experiment/dumper/models.py
@@ -4,7 +4,6 @@ from typing import Literal
 
 import lightning.pytorch as pl
 import torch
-import wandb
 from safetensors.torch import save_model
 
 
@@ -22,24 +21,23 @@ def save_json(path: Path, data: dict):
         json.dump(data, f)  # type: ignore
 
 
-def save_fn(
-    name: str,
+def save_lightning(
+    path: str | Path,
     model: pl.LightningModule,
     method: Literal["pt", "safetensors"] = "safetensors",
     save_hparams: bool = False,
 ):
     """
-    :param name: name of the parent directory with the model and settings
+    :param path: name of the parent directory with the model and settings
     :param model: PyTorch model
     :param method: method of saving the model, either "pt" or "safetensors"
     :param save_hparams: whether to save hyperparameters in a JSON file
     """
-    path = Path(f"models/{name}")
+    if isinstance(path, str):
+        path = Path(path)
     path.mkdir(parents=True, exist_ok=True)
 
     save_torch(path / f"trained-model.{method}", model=model.module, method=method)  # save only the model
 
     if save_hparams:
         save_json((path / "hparams.json"), model.hparams or {})
-
-    wandb.save(path)
